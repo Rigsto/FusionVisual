6783477740a4b48b6b769e873f9164cbe7fd6768
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2019-05-12 15:18:00 +0700

    checkout form update

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -5,17 +5,47 @@
             <h2>Checkout</h2>
         </div>
     </section>
-    <section id="coutform" style="margin-top: 7em;">
+    <section id="coutform" class="py-5">
         <div class="container">
             <div class="row no-gutters">
                 <div class="col-md-6">
                     <h4>Customer Details</h4>
-                    <form>
-                        <div class="form-group"><label>Email Address</label><input class="form-control form-control" type="email"></div>
-                        <h4>Payment Details</h4>
-                        <div class="form-group"><label>Bank Transfer</label><select class="custom-select"><option value="">Choose Bank</option><option value="" selected="">BCA</option></select></div>
-                        <div class="form-group"><button class="btn btn-primary" type="button">Complete Order</button></div>
-                    </form>
+                    {!! Form::open(['method'=>'POST']) !!}
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label>Email Address</label>
+                        @if($user = \Illuminate\Support\Facades\Auth::user())
+                            {!! Form::email('email', $user->email, ['class'=>'form-control', 'readonly', 'placeholder'=>'E-mail Address...']) !!}
+                            <span class="text-primary">Your email has filled automaticaly because your are logged in.</span>
+                        @else
+                            {!! Form::email('email', null, ['class'=>'form-control', 'required'=>'', 'placeholder'=>'E-mail Address...']) !!}
+                        @endif
+                    </div>
+                    @guest
+                    <div class="form-group">
+                        <label>Full Name</label>
+                        {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'Full Name..' , 'required'=>'']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        {!! Form::password('password', ['class'=>'form-control', 'placeholder'=>'Password...' , 'required'=>'']) !!}
+                    </div>
+                    <div class="form-group">
+                        <label>Re-type Password</label>
+                        {!! Form::password('password_confirmation', ['class'=>'form-control', 'placeholder'=>'Re-type Password...' , 'required'=>'']) !!}
+                    </div>
+                    @endguest
+                    <h4>Payment Details</h4>
+                    <div class="form-group">
+                        <label>Bank Transfer</label>
+                        {!! Form::select('payment', array('Choose Bank',
+                                            'BCA'),
+                            null, ['class'=>'custom-select', 'id' => 'mplace']) !!}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::submit('Complete Order', ['class'=>'btn btn-primary']) !!}
+                    </div>
+                    {!! Form::close() !!}
                 </div>
                 <div class="col-md-4 offset-xl-2">
                     <h4 style="margin: 0;">Your Order</h4>
